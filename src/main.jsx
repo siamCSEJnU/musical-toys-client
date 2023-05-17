4a81920e947f6cb014bfdba45c1437528a44d811
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-05-18 01:42:27 +0600

    navigation bar added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-7xl mx-auto">
+    <React.StrictMode>
+      <RouterProvider router={router}></RouterProvider>
+    </React.StrictMode>
+  </div>
+);
---
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import navLogo from "../../../assets/navLogo.jpg";
+
+const NavigationBar = () => {
+  return (
+    <div className="md:flex justify-between items-center md:h-32 p-3  bg-orange-300 space-y-5 md:space-y-0 ">
+      <div>
+        <div className="flex items-center gap-2 ">
+          <img src={navLogo} className="h-28 rounded-full" alt="navLogo" />
+          <h2 className=" font-extrabold text-5xl font-serif">
+            Musical <br /> Toys
+          </h2>
+        </div>
+      </div>
+      <div>
+        <ul className=" md:flex gap-5 font-bold text-xl space-y-2 md:space-y-0">
+          <li>Home</li>
+          <li>All Toys</li>
+          <li>My Toys</li>
+          <li>Add A Toy</li>
+          <li>Blogs</li>
+        </ul>
+      </div>
+      <div className="text-center font-bold text-xl">
+        <button>Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default NavigationBar;
